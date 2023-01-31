dd3779010445258ff8b045ae9628398529bfed67
Manuel Frischknecht <manuel.frischknecht@gmail.com>
2023-01-31 21:15:23 +0100

    Fix stack traces logged from scripts

---
+++ assets/index-15577627.js
@@ -0,0 +1,4 @@
+import{e,d as r}from"./index-d8f01f51.js";import"./editorWorker-7b78be25.js";import"./jsonWorker-4fdea8cc.js";import"./cssWorker-3bf0cd15.js";import"./htmlWorker-5f0f4143.js";import"./tsWorker-c8da3dc3.js";let n=e,s=r,a=`
+    export function encode(bytes: ArrayBuffer): string;
+    export function decode(base64: string): ArrayBuffer;
+`;export{s as decode,n as encode,a as typescriptDeclarations};
