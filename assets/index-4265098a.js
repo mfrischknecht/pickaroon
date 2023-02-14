e9bf7cf63727067f173ee8dfb92ef844e28b534d
Manuel Frischknecht <manuel.frischknecht@gmail.com>
2023-02-14 13:08:08 +0100

    Fix declarations of optional properties in message types

--- assets/index-4265098a.js
+++
@@ -1,4 +0,0 @@
-import{e,d as r}from"./index-897cd0f8.js";import"./editorWorker-7b78be25.js";import"./jsonWorker-4fdea8cc.js";import"./cssWorker-3bf0cd15.js";import"./htmlWorker-5f0f4143.js";import"./tsWorker-c8da3dc3.js";let n=e,s=r,a=`
-    export function encode(bytes: ArrayBuffer): string;
-    export function decode(base64: string): ArrayBuffer;
-`;export{s as decode,n as encode,a as typescriptDeclarations};
