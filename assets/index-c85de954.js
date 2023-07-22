fcf3d40607abfa5ef32186bdff3ab7ca64e7b97c
Manuel Frischknecht <manuel.frischknecht@gmail.com>
2023-07-22 19:48:48 +0200

    Upgrade dependencies

---
+++ assets/index-c85de954.js
@@ -0,0 +1,4 @@
+import{e,d as r}from"./index-37631b04.js";import"./editorWorker-ea04ce91.js";import"./jsonWorker-529255a1.js";import"./cssWorker-bac91942.js";import"./htmlWorker-07a07472.js";import"./tsWorker-23cb663e.js";let n=e,s=r,a=`
+    export function encode(bytes: ArrayBuffer): string;
+    export function decode(base64: string): ArrayBuffer;
+`;export{s as decode,n as encode,a as typescriptDeclarations};
