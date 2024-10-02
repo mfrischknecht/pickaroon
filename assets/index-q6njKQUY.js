39276e026301178cf4ccadf13d62825fcf35fec1
Manuel Frischknecht <manuel.frischknecht@gmail.com>
2024-10-03 00:42:34 +0200

    Update dependencies

---
+++ assets/index-q6njKQUY.js
@@ -0,0 +1,4 @@
+import{h as r,o as n}from"./index-DDimtgep.js";import"./editorWorker-HNH1Zuqw.js";import"./jsonWorker-DPjOesRx.js";import"./cssWorker-Duxp3b6W.js";import"./htmlWorker-Bsd5yFiF.js";import"./tsWorker-BCXOvNBb.js";function e(t){return r(t)}function c(t){return n.sha1(t)}let u=`
+    export function xxhash(input: any): string;
+    export function sha1  (input: any): string;
+`;export{c as sha1,u as typescriptDeclarations,e as xxhash};
